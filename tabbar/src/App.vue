cc78e68e02b343c6d0b7854f5233cba5ea33afa9
machinesily <934229118@qq.com>
2020-12-04 15:42:11 +0800

    2020/12/4 15:41

---
+++ tabbar/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  <div id="tab-bar">
+    <div id="tab-bar-item">首页</div>
+    <div id="tab-bar-item">分类</div>
+    <div id="tab-bar-item">购物车</div>
+    <div id="tab-bar-item">我的</div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+@import './assets/css/base.css';
+#tab-bar{
+  display: flex;
+}
+#tab-bar-item{
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
